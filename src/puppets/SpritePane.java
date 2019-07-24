a33051477c1b7c2ac5672039ece5ad83758fe39e
Jeff Martin <jeff@reportmill.com>
2019-07-24 15:07:52 -0500

    SpritePane: Added PlayLoop button and Flip Image switch

--- src/puppets/SpritePane.java
+++ src/puppets/SpritePane.java
@@ -19,6 +19,12 @@ public class SpritePane extends ViewOwner {
     
     // A ListView to show actions
     ListView <PuppetAction>  _actionList;
+    
+    // Whether to flip image
+    boolean                  _flipImage;
+    
+    // Whether to loop anim
+    boolean                  _loopAnim;
 
 /**
  * Creates SpritePane.
@@ -46,6 +52,8 @@ protected void initUI()
     _actionList.setItemTextFunction(action -> { return action.getName(); });
     _actionList.setItems(_actions.getActions());
     _actionList.setSelIndex(0);
+    
+    getUI().addPropChangeListener(pc -> setSpriteImage(), View.Showing_Prop);
 }
 
 /**
@@ -53,9 +61,26 @@ protected void initUI()
  */
 protected void respondUI(ViewEvent anEvent)
 {
+    // Handle ActionList
+    if(anEvent.equals("ActionList"))
+        setSpriteImage();
+        
     // Handle PlayButton
     if(anEvent.equals("PlayButton"))
+        playAnim();
+        
+    // Handle PlayButton
+    if(anEvent.equals("PlayLoopButton")) {
+        _loopAnim = anEvent.getBoolValue();
+        if(_loopAnim) playAnim();
+        else stopAnim();
+    }
+        
+    // Handle FlipImageSwitch
+    if(anEvent.equals("FlipImageSwitch")) {
+        _flipImage = anEvent.getBoolValue();
         setSpriteImage();
+    }
 }
 
 /**
@@ -66,13 +91,48 @@ protected void setSpriteImage()
     Puppet puppet = _docPane._puppet;
     PuppetAction action = _actionList.getSelItem();
     Image img = getImage(puppet, action);
+    if(_flipImage)
+        img = getImagesFlipped(img);
     
-    ImageView iview = new ImageView(img);
+    // Create/set new ImageView
+    _imgView = new ImageView(img);
     BoxView pupBox = getView("PuppetBox", BoxView.class);
-    pupBox.setContent(iview);
+    pupBox.setContent(_imgView);
     
+    // Play anim
+    playAnim();
+}
+
+/**
+ * Play anim.
+ */
+void playAnim()
+{
+    PuppetAction action = _actionList.getSelItem();
     int time = action.getMaxTime();
-    iview.getAnim(time).setValue("Frame", img.getImageSet().getCount()).play();
+    _imgView.getAnimCleared(time).setValue("Frame", _imgView.getFrameCount()).setLinear().play();
+    if(_loopAnim)
+        _imgView.getAnim(0).setOnFinish(a -> animFinished());
+}
+
+/**
+ * Stop Anim.
+ */
+void stopAnim()
+{
+    _imgView.getAnimCleared(0).stop();
+    _imgView.setFrame(0);
+}
+
+/**
+ * Called when anim is done.
+ */
+void animFinished()
+{
+    if(_loopAnim) {
+        playAnim();
+        //_imgView.setAnimTimeDeep(200);
+    }
 }
 
 /**
@@ -100,6 +160,34 @@ public Image getImage(Puppet aPuppet, PuppetAction anAction)
     ImageSet imgSet = new ImageSet(images);
     Image img = imgSet.getImage(0);
     return img;
+}
+
+/**
+ * Returns the flipped image.
+ */
+public Image getImageFlipped(Image anImage)
+{
+    int w = (int)Math.round(anImage.getWidth()), h = (int)Math.round(anImage.getHeight());
+    Image img = Image.get(w, h, anImage.hasAlpha());
+    Painter pntr = img.getPainter();
+    Transform xfm = new Transform(w/2, h/2); xfm.scale(-1, 1); xfm.translate(-w/2, -h/2);
+    pntr.transform(xfm);
+    pntr.drawImage(anImage, 0, 0);
+    return img;
+}
+
+/**
+ * Returns the flipped image.
+ */
+public Image getImagesFlipped(Image anImage)
+{
+    ImageSet iset = anImage.getImageSet();
+    int count = iset.getCount();
+    List <Image> imgs2 = new ArrayList(count);
+    for(int i=0;i<count;i++) { Image img = iset.getImage(i);
+        imgs2.add(getImageFlipped(img)); }
+    ImageSet iset2 = new ImageSet(imgs2);
+    return iset2.getImage(0);
 }
 
 }
